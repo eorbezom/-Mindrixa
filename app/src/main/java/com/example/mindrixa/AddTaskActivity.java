6464caa39d5d3a26ed69d343a4ffd0b8e247f029
eorbezom <eorbezo99@gmail.com>
2024-10-13 00:10:36 -0500

    solo se sube el comentario

--- app/src/main/java/com/example/mindrixa/AddTaskActivity.java
+++ app/src/main/java/com/example/mindrixa/AddTaskActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
+/*hola mundo*/
 
 public class AddTaskActivity extends AppCompatActivity {
 
